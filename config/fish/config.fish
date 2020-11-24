949670ef231228df4d29dae0769d20f8f34e4cc0
Robert Ellen <rellen@ieee.org>
2020-11-24 12:12:08 +1000

    Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,11 @@
 set -Ux EDITOR vim
 
 ## aliases
+alias ls "/usr/local/opt/coreutils/libexec/gnubin/ls --color=auto"
+alias ll "ls -la"
+alias lash "ls -lash"
+
+alias python "python3"
 
 alias gs "git status"
 alias ga "git add"
@@ -18,6 +23,19 @@ alias ips "iex -S phx.server"
 alias gx "gigalixir"
 alias gxl "gigalixir logs"
 
+## source nix script so that nix-* commands are available
+if test -e ~/.nix-profile/etc/profile.d/nix.sh
+  bass source ~/.nix-profile/etc/profile.d/nix.sh
+end
+
+## suport asdf
+source /usr/local/opt/asdf/asdf.fish
+
+## save elixir history
+set -Ux ERL_AFLAGS "-kernel shell_history enabled"
+
 ## fix up paths to prefer brew etc
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "/usr/local/bin" $fish_user_paths
+set -g fish_user_paths "/Users/robert/.mix/escripts" $fish_user_paths
+
